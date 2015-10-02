00e81e335d3305d90b11c75928b3f5a640a4b8a5
VeteranPadgett <batesev53@gmail.com>
2015-10-01 19:39:21 -0500

    Add initial files
    Login screen. Also a place order and view order screen, but these aren't
    functional yet.

---
+++ login.js
@@ -0,0 +1,15 @@
+var username;
+
+$(document).ready(function() {
+    $("#login-button").click(function() {
+        username = $("input[name=username]").val();
+        if(username == "customer")
+        {
+            location.href = "place_order.html";
+        }
+        else if(username == "manager")
+        {
+            location.href = "orders.html";
+        }
+    });
+});
